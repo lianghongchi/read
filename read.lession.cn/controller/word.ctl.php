906924993fcfbcf43d38d11fddfa2a660ee59abb
15140289758 <bleach76517655@163.com>
2020-05-05 11:33:50 +0800

    计算高考英语单词的出现次数

--- read.lession.cn/controller/word.ctl.php
+++ read.lession.cn/controller/word.ctl.php
@@ -1,6 +1,18 @@
 <?php
 class word_controller extends controller_lib {
 
+    /**
+     * 是否需要登录账号
+     * @var bool
+     */
+    protected $_isLoginRequired = false;
+
+    /**
+     * 是否加入权限控制
+     * @var bool
+     */
+    protected $_isAuthControl = false;
+
     public function index_action() {
         lib_word_operation::operation();
     }
