bac78944f34ab7fac5cab33f0fb0c0a977ff1bfc
15140289758 <bleach76517655@163.com>
2020-05-05 11:58:45 +0800

    计算高考英语单词的出现次数

--- kis_lib/library/word/operation.lib.php
+++ kis_lib/library/word/operation.lib.php
@@ -4,10 +4,14 @@ class lib_word_operation {
     private static $redisPre = 'word_';
     private static $redisTimeout = 86400;
 
+    private static $file = ['a.txt'];
+
     public static function operation() {
         $r = new r(self::$redisGroup);
-        var_dump($r);
-        echo 1;
+        foreach (self::$file as $item) {
+            $arr = file('./'. $item);
+            print_r($arr);
+        }
         exit();
     }
 }
